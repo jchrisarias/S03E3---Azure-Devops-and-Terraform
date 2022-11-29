de868d179b2260b306924646cf9730a61859aebf
jchrisarias <81653707+jchrisarias@users.noreply.github.com>
2022-11-29 17:37:41 -0600

    Updated Pipeline File..@

--- main.tf
+++ main.tf
@@ -11,6 +11,15 @@ provider "azurerm" {
 features {}
 }
 
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_storage"
+        storage_account_name = "tfstoragejchrisarias"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location  = "East US"
