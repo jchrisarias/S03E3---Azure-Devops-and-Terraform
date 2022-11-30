be2da676ba44fbda541c9c06ecc48172f649c04d
jchrisarias <81653707+jchrisarias@users.noreply.github.com>
2022-11-30 12:27:45 -0600

    Final Commit Pipeline...

--- main.tf
+++ main.tf
@@ -26,25 +26,25 @@ resource "azurerm_resource_group" "tf_test" {
     location  = "East US"
 }
 
-//resource "azurerm_container_group" "tfcg_test" {
-//    name = "weatherapi"
-//    location = azurerm_resource_group.tf_test.location
-//    resource_group_name = azurerm_resource_group.tf_test.name
+resource "azurerm_container_group" "tfcg_test" {
+    name = "weatherapi"
+    location = azurerm_resource_group.tf_test.location
+    resource_group_name = azurerm_resource_group.tf_test.name
     
-//    ip_address_type = "Public"
- //   dns_name_label = "jchrisariaswa"
- //   os_type = "Linux"
+    ip_address_type = "Public"
+    dns_name_label = "jchrisariaswa"
+    os_type = "Linux"
 
-//  container {
-//    name = "wheatherapi"
-//    image = "jchrisarias/wheatherapi"
-  //  cpu = "1"
-    //memory = "1"
+  container {
+    name = "wheatherapi"
+    image = "jchrisarias/wheatherapi"
+    cpu = "1"
+    memory = "1"
 
-    //ports {
-      //port = "80"
-      //protocol = "TCP"
-    //}
+    ports {
+      port = "80"
+      protocol = "TCP"
+    }
 
-  //}
-//}
+  }
+}
